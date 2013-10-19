0b1e07ad53d864eb6d8797be12c2b0c0105d1cc9
Brian Truong <btruong2@illinois.edu>
2013-10-19 18:08:09 -0500

    Added basic GUI stuff
    GUITaskPanel and Task classes are still seperate for now. Run the main
    inside GUIFrame to test it out.

---
+++ src/GUIFrame.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GUIFrame extends JFrame
+{
+  JMenuBar menuBar;
+  JMenu menu;
+  JMenuItem menuItem;
+
+  JScrollPane scrollPane;
+  JPanel rootPanel;
+  JPanel mainPanel;
+
+  public GUIFrame()
+  {
+    super("SAB Project Manager");
+    setBounds(100,100,300,200);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    menuBar = new JMenuBar();
+    menu = new JMenu("A Menu");
+    menuItem = new JMenuItem("A text-only menu item");
+    menu.add(menuItem);
+    menuBar.add(menu);
+    setJMenuBar(menuBar);
+
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+    GUITaskPanel p = new GUITaskPanel();
+    mainPanel.add(p);
+    for (int i = 0; i < 10; i++)
+    {
+      p = new GUITaskPanel();
+      mainPanel.add(p);
+    }
+
+    scrollPane = new JScrollPane(mainPanel);
+
+    rootPanel = (JPanel) getContentPane();
+    rootPanel.add(scrollPane);
+
+    pack();
+    setVisible(true);
+  }
+
+  public static void main(String[] args)
+  {
+    GUIFrame g = new GUIFrame();
+  }
+}
---
+++ src/GUITaskPanel.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GUITaskPanel extends JPanel
+{
+  JRadioButton a;
+  JRadioButton b;
+  JRadioButton c;
+  ButtonGroup grp;
+
+  JLabel lbl;
+
+  public GUITaskPanel()
+  {
+    super();
+    setAlignmentX(JComponent.CENTER_ALIGNMENT);
+    setPreferredSize(new Dimension(500, 50));
+    setMaximumSize(new Dimension(500, 50));
+
+    ImageIcon ia = new ImageIcon("res/incomplete.png", "Incomplete");
+    ImageIcon ib = new ImageIcon("res/work.png", "Working");
+    ImageIcon ic = new ImageIcon("res/complete.png", "Complete");
+    a = new JRadioButton("Incomplete");
+    b = new JRadioButton("Working");
+    c = new JRadioButton("Complete");
+
+    grp = new ButtonGroup();
+    grp.add(a);
+    grp.add(b);
+    grp.add(c);
+
+    add(new JLabel(ia));
+    add(a);
+    add(new JLabel(ib));
+    add(b);
+    add(new JLabel(ic));
+    add(c);
+
+    lbl = new JLabel("Task 1");
+    add(lbl);
+  }
+}
